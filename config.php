77509dd028fb2933691289612a3f7af1e5891168
Estevao <estevaom404@gmail.com>
2021-08-28 10:52:10 -0300

    criando tabela e conexão com o banco de dados

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$dsn = "mysql:dbname=controleusuarios;host=localhost";
+$dbuser = "root";
+$dbpass = "";
+
+try {
+	$pdo = new PDO($dsn, $dbuser, $dbpass);
+
+} catch (PDOException $e) {
+	echo "Falhou a conexão: ".$e->getMessage();
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require 'config.php';
+?>
+<table border="1px" width="100%">
+	<tr>
+		<th>Nome</th>
+		<th>E-mail</th>
+		<th>Ações</th>
+	</tr>
+</table>
