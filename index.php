ebb436a505c1e5cd1615c762783983e0ee79263f
Estevao <estevaom404@gmail.com>
2021-08-28 11:45:24 -0300

    preenchendo a tabela com os dados do banco de dados

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
 require 'config.php';
 ?>
+<a href="adicionar.php">Adicionar Novo Usuário</a>
 <table border="1px" width="100%">
 	<tr>
 		<th>Nome</th>
 		<th>E-mail</th>
 		<th>Ações</th>
 	</tr>
+	<?php
+	$sql = "SELECT * FROM usuarios";
+	$sql = $pdo->query($sql);
+
+	if($sql->rowCount() == 0){
+		echo '<tr>';
+			echo '<td> Vazio </td>';
+			echo '<td> Vazio </td>';
+			echo '<td> Nenhuma </td>';
+		echo '</tr>';
+	}
+	if($sql->rowCount() > 0){
+		foreach ($sql->fetchAll() as $usuario) {
+			echo '<tr>';
+				echo '<td>'.$usuario['nome'].'</td>';
+				echo '<td>'.$usuario['email'].'</td>';
+				echo '<td> <a href="editar.php?id='.$usuario['id'].' "> Editar </a> - <a href="excluir.php?id='.$usuario['id'].' "> Excluir </a> </td>';
+			echo '</tr>';
+		}
+	}
+	?>
 </table>
