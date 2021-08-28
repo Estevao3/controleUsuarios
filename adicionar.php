25a7b48a2eebf93dc131331b89290d118eba21d2
Estevao <estevaom404@gmail.com>
2021-08-28 11:51:44 -0300

    criando formulario para adicionar email e usuario

---
+++ adicionar.php
@@ -0,0 +1,10 @@
+<form>
+	Nome:<br>
+	<input type="text" name="nome"><br><br>
+	E-mail:<br>
+	<input type="text" name="email"><br><br>
+	Senha:<br>
+	<input type="password" name="senha"><br><br>
+
+	<input type="submit" value="Cadastrar">
+</form>
