3c73d9f65c099ffd26672a5dc1b425a4bd5aeb91
Estevao <estevaom404@gmail.com>
2021-08-28 12:18:56 -0300

    criando a parte que envia para o banco de dados

--- adicionar.php
+++ adicionar.php
@@ -1,4 +1,17 @@
-<form>
+<?php
+require 'config.php';
+
+if(isset($_POST['nome']) && empty($_POST['nome']) == false){
+	$nome = addslashes($_POST['nome']);
+	$email = addslashes($_POST['email']);
+	$senha = md5(addslashes($_POST['senha']));
+
+	$sql = "INSERT INTO usuarios SET nome = '$nome', email = '$email', senha = '$senha'";
+	$pdo->query($sql);
+	header("Location: index.php");
+}
+?>
+<form method="POST">
 	Nome:<br>
 	<input type="text" name="nome"><br><br>
 	E-mail:<br>
